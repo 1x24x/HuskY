6e1848383591e586045d9e6d27d050f952b34bb3
1x24x <89283735+1x24x@users.noreply.github.com>
2023-01-03 23:53:18 +0000

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'.Haris#9108','/prediction','/varity','immortal','best dev','crypto mastermind','paster',],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
